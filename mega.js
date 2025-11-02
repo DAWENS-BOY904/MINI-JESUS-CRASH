47b8d3453174b11ee258cfdddec3202c24a30942
GOD-Da <dawiniefleury17@gmail.com>
2025-11-02 16:02:55 -0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import { Storage } from "megajs";
 
 const auth = {
-    email: 'goatvillano480@gmail.com',
-    password: 'Lens122&@',
+    email: 'clay_exurban.6l@icloud.com',
+    password: 'Jetne7-javkuz-nofnyr',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
